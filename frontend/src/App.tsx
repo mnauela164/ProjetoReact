657cf28586550e0938554d3ebedfd2243967a3e0
mnauela164 <manueladias0204@gmail.com>
2026-08-11 11:38:39 -0300

    feat: add routing and pages to React application; include Prettier and Tailwind CSS support

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,26 @@
+import { Link, Routes, Route } from "react-router";
+
 import "./App.css";
+import Home from "./pages/Home";
+import Produtos from "./pages/Produtos";
+import Contato from "./pages/Contato";
 
 function App() {
   return (
-    <main className="flex items-center justify-center bg-red-200 min-h-screen">
-    <div>
-       <h1>Hello World!!!</h1>
-       <p>Bem vindo ao meu React com Tailwind CSS!</p>
-    </div>
-  </main>
+    <main className="min-h-screen bg-slate-300">
+      <nav>
+        <Link to={"/"}>Home</Link>
+        <Link to="/produtos">Produtos</Link>
+        <Link to="/contato">Contato</Link>
+      </nav>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/produtos" element={<Produtos />} />
+          <Route path="/contato" element={<Contato />} />
+        </Routes>
+      </div>
+    </main>
   );
 }
 
---
+++ frontend/src/pages/Contato.tsx
@@ -0,0 +1,7 @@
+function Contato () {
+    return (
+    <h1 className="text-5xl font-bold text-blue-800">Contato</h1>
+    )
+}
+
+export default Contato;
